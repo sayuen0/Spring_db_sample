51b4b2234e5dad38c1e522576abe7187c369a48d
Akira Kosuda <sayuen0@gmail.com>
2018-12-30 14:16:53 +0900

    thymeleaf 繰り返し処理を確認

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,6 +1,9 @@
 
 package hello;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +15,36 @@ public class HelloController {
   public String hello(Model model) {
     model.addAttribute("hoge", new Hoge()).addAttribute("modelValue", "Model Value");
     return "hello";
+  }
+
+  @GetMapping("/greet")
+  public String greet(Model model) {
+    Greeting greet = new Greeting(1, "Hello thymeleaf!!");
+    model.addAttribute("id", greet.getId()).addAttribute("content", greet.getContent());
+    return "greet";
+  }
+
+  @GetMapping("/map")
+  public String map(Model model) {
+    HashMap<String, String> map = new HashMap<>();
+    map.put("message", "hello world!");
+
+    model.addAttribute("map", map);
+    return "map";
+  }
+
+  @GetMapping("/each")
+  public String each(Model model) {
+    // Listをadd
+    model.addAttribute("list", Arrays.asList("hoge", "huga", "poyo"));
+    // Mapをadd
+    HashMap<String, String> map = new HashMap<>();
+    map.put("hoge", "HOGE");
+    map.put("fuga", "FUGA");
+    map.put("piyo", "PIYO");
+    model.addAttribute("map", map);
+    return "each";
+
   }
 
   public static class Hoge {
